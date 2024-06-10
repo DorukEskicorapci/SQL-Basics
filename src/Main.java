cf7721ee9f1a92164031d78c8467174345e6a3e9
Doruk Eskicorapci <de295@rutgers.edu>
2024-06-10 18:17:14 +0300

    Working v1

---
+++ src/Main.java
@@ -0,0 +1,129 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class Main {
+    static final String URL = "jdbc:sqlite:db.db";
+
+
+
+    // "INSERT INTO Remuneraciones (Nombre, Apellido, Rut, Edad, Tiempo, Sueldo) VALUES ("+NAME+" ,"+LNAME, ID )+ ", " + AGE + ", " + TIME + ", " + SALARY + ");";
+
+    // static final String URL = "jdbc:sqlite:C:/Users/doruk/OneDrive/Documents/Collage life/BTC Staj/Staj/SQL/SQL Project 1/db.db";
+
+    static void operator() throws SQLException {
+
+
+        System.out.println("1.Select \n2.Insert \n3.Delete \n4.Update \n5.Exit ");
+        System.out.print("\nEnter command number or name: ");
+
+
+        Scanner sc = new Scanner(System.in);
+        Integer input = sc.nextInt();
+        boolean count = false;
+
+        while (input < 5){
+
+            if (count == true){
+
+                System.out.println("===========================================================================");
+                operator();
+
+            } else {
+                count = true;
+            }
+
+
+            if (input == 1) {
+
+                System.out.println();
+                sqlcommands.select();
+
+            } else if (input == 2) {
+
+                System.out.print("\nType the username that will be assigned to your data: ");
+                String usernameinut = sc.next();
+                System.out.print("Type the name that will be assigned to your data: ");
+                String nameinput = sc.next();
+                System.out.print("Type the surname that will be assigned to your data: ");
+                String surnameinput = sc.next();
+                System.out.print("Type the age that will be assigned to your data: ");
+                Integer ageinput = sc.nextInt();
+                System.out.print("\n");
+                sqlcommands.insert(usernameinut,nameinput,surnameinput,ageinput);
+
+            } else if (input == 3) {
+
+                System.out.print("\nType the username assigned to data you want to delete: ");
+
+                String deleteinput = sc.nextLine();
+
+                sqlcommands.delete(deleteinput);
+                System.out.print("\n");
+
+            } else if (input == 4) {
+                System.out.print("\nType the username assigned to data you want to update: ");
+
+                String updateinput = sc.next();
+                sqlcommands.update(updateinput);
+
+            } else if (input == 5) {
+                break;
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        driver();
+    }
+
+    static void driver() throws SQLException {
+
+        Connection connection = null;
+        connection = DriverManager.getConnection(URL);
+        Scanner sc = new Scanner(System.in);
+
+        try{
+            connection = getConnection();
+            System.out.println("Connecting...");
+            Statement statement = connection.createStatement();
+
+
+            boolean checkagain = false;
+
+            try {
+                if (connection != null) {
+                    System.out.println("Connected and running \n");
+
+                    //static final String URL = "jdbc:sqlite:db.db";
+
+
+                    operator();
+
+
+                }
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+                System.out.print("\n1. Yes \n2. No" + "\nDo you want to try connect again?: ");
+
+                String input = sc.nextLine();
+
+                if (input.equalsIgnoreCase("1") || input.equalsIgnoreCase("yes") || input.equalsIgnoreCase("1.yes")) {
+                    driver();
+                } else if (input.equalsIgnoreCase("2") || input.equalsIgnoreCase("no") || input.equalsIgnoreCase("2.no")) {};
+
+            }
+        }catch (Exception e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+
+    public static Connection getConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("org.sqlite.JDBC");
+        return DriverManager.getConnection(URL);
+    }
+
+}
---
+++ src/sqlcommands.java
@@ -0,0 +1,234 @@
+import java.sql.*;
+import java.util.Scanner;
+
+public class sqlcommands {
+    static final String URL = "jdbc:sqlite:db.db";
+
+
+
+    static void insert(String username_in, String name_in, String surname_in, Integer age_in) throws SQLException {
+        Connection connection = null;
+        connection = DriverManager.getConnection(URL);
+        Statement statement = connection.createStatement();
+        boolean checkbox = true;
+
+        username_in = "'" + username_in + "'";
+        name_in = "'" + name_in + "'";
+        surname_in = "'" + surname_in + "'";
+        age_in = age_in;
+        String checkbox_in =  "'" + checkbox + "'";
+
+        String values = surname_in + name_in + surname_in + age_in;
+
+        System.out.println("Intended input summary: " + username_in + " " + name_in + " " + surname_in + " '" + age_in + "'" );
+
+
+        String data = "INSERT INTO users(username, name, surname, age, checkbox) VALUES (" + username_in + ", " + name_in + ", " + surname_in + ", " + age_in + ", " + checkbox_in + ");";
+        statement.executeUpdate(data);
+        statement.close();
+        System.out.println("User inserted successfully \n");
+
+    }
+
+    static void select() throws SQLException {
+        Connection connection = null;
+        connection = DriverManager.getConnection(URL);
+
+        String data;
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users;");
+
+        System.out.printf("%-10s %-20s %-20s %-20s %-5s", "id", "username", "name", "surname", "age");
+        System.out.print("\n ---------------------------------------------------------------------------");
+
+        while(resultSet.next()) {
+            System.out.println();
+            System.out.printf("%-10s %-20s %-20s %-20s %-5s",
+            resultSet.getInt("id"),
+            resultSet.getString("username"),
+            resultSet.getString("name"),
+            resultSet.getString("surname"),
+            resultSet.getInt("age")
+            );
+        }
+
+        System.out.println("\n");
+    }
+
+
+    static void delete(String username_in) throws SQLException {
+        Connection connection = null;
+        connection = DriverManager.getConnection(URL);
+        Statement statement = connection.createStatement();
+        Scanner sc = new Scanner(System.in);
+
+        String name_in = "";
+        String surname_in = "";
+        int age_in = 0;
+
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users;");
+
+        while(resultSet.next()) {
+            if(resultSet.getString("username").equals(username_in)) {
+               name_in = resultSet.getString("name");
+               surname_in = resultSet.getString("surname");
+               age_in = resultSet.getInt("age") ;
+            }
+        }
+
+        username_in = "'" + username_in + "'";
+        name_in = "'" + name_in + "'";
+        surname_in = "'" + surname_in + "'";
+
+        Integer check = 2;
+
+        while (check == 2) {
+            System.out.print("Intended data summary: " + username_in + " " + name_in + " " + surname_in + " '" + age_in + "'" + "\n");
+            System.out.print("\n1.Yes\n2.No" + "\nIs this account you?: ");
+            Integer check2 = sc.nextInt();
+            check = check2;
+            if (check == 1) {
+                String data = "DELETE FROM users WHERE username =" + username_in + ";";
+                statement.executeUpdate(data);
+            } else if (check == 2) {
+
+                System.out.print("\nType the username again: ");
+                String deleteinput = sc.next();
+                ResultSet resultSet2 = connection.createStatement().executeQuery("SELECT * FROM users;");
+
+                while (resultSet2.next()) {
+                    if (resultSet2.getString("username").equals(deleteinput)) {
+                        username_in = "'"+ deleteinput+"'";
+                        name_in = resultSet2.getString("name");
+                        surname_in = resultSet2.getString("surname");
+                        age_in = resultSet2.getInt("age");
+                    } else {
+                        username_in = "NA";
+                        name_in = "";
+                        surname_in = "";
+                        age_in = 0;
+                    }
+                }
+            }
+        }
+        System.out.println("Deleted user: " + username_in);
+        statement.close();
+    }
+
+    static void update(String username_in) throws SQLException {
+        Connection connection = null;
+        connection = DriverManager.getConnection(URL);
+        Statement statement = connection.createStatement();
+        Scanner sc = new Scanner(System.in);
+
+        String name_in = "";
+        String surname_in = "";
+        int age_in = 0;
+
+        ResultSet resultSet = connection.createStatement().executeQuery("SELECT * FROM users;");
+
+        while(resultSet.next()) {
+            if(resultSet.getString("username").equals(username_in)) {
+                name_in = resultSet.getString("name");
+                surname_in = resultSet.getString("surname");
+                age_in = resultSet.getInt("age") ;
+            }
+        }
+
+        username_in = "'" + username_in + "'";
+        name_in = "'" + name_in + "'";
+        surname_in = "'" + surname_in + "'";
+
+        Integer check = 2;
+
+        while (check == 2) {
+            System.out.print("Intended data summary: " + username_in + " " + name_in + " " + surname_in + " '" + age_in + "'" + "\n");
+            System.out.print("\n1.Yes\n2.No" + "\nIs this account you?: ");
+            Integer check2 = sc.nextInt();
+            check = check2;
+            String data;
+            if (check == 1) {
+
+                System.out.print("\n1.Username\n2.Name\n3.Surname\n4.Age" + "\nWhich value you want to change?: ");
+                Integer ask = sc.nextInt();
+
+
+                switch (ask){
+                    case 1:{
+                        System.out.print("\nEnter the new value: ");
+                        String newvalue = sc.next();
+                        username_in = newvalue;
+                        data = "UPDATE users SET username = '" + newvalue + "' WHERE username = '" + username_in + "';";
+                        statement.executeUpdate(data);
+                        System.out.println("Changed user: " + username_in);
+                        System.out.print("New data summary: " + username_in + " " + name_in + " " + surname_in + " '" + age_in + "'" + "\n");
+                        break;
+                    }
+                    case 2: {
+                        System.out.print("\nEnter the new value: ");
+                        String newvalue = sc.next();
+                        data = "UPDATE users SET name = '" + newvalue + "' WHERE username = '" + username_in + "';";
+                        statement.executeUpdate(data);
+                        name_in = newvalue;
+                        System.out.println("Changed user: " + username_in);
+                        System.out.print("New data summary: " + username_in + " " + name_in + " " + surname_in + " '" + age_in + "'" + "\n");
+                        break;
+                    }
+                    case 3: {
+                        System.out.print("\nEnter the new value: ");
+                        String newvalue = sc.next();
+                        data = "UPDATE users SET surname = '" + newvalue + "' WHERE username = '" + username_in + "';";
+                        statement.executeUpdate(data);
+                        surname_in = newvalue;
+                        System.out.println("Changed user: " + username_in);
+                        System.out.print("New data summary: " + username_in + " " + name_in + " " + surname_in + " '" + age_in + "'" + "\n");
+                        break;
+                    }
+                    case 4: {
+                        System.out.print("\nEnter the new value: ");
+                        String newvalue = sc.next();
+                        data = "UPDATE users SET age = '" + newvalue + "' WHERE username = '" + username_in + "';";
+                        statement.executeUpdate(data);
+                        age_in = resultSet.getInt(newvalue);
+                        System.out.println("Changed user: " + username_in);
+                        System.out.print("New data summary: " + username_in + " " + name_in + " " + surname_in + " '" + age_in + "'" + "\n");
+                        break;
+                    }
+                    default: {
+                        System.out.println("Invalid value");
+                        break;
+                    }
+                }
+
+                //String data = "UPDATE users SET name = 'Alfred Schmidt', surname= 'Frankfurt' WHERE username = 'doruk';";
+
+
+
+            } else if (check == 2) {
+
+                System.out.print("\nType the username again: ");
+                String deleteinput = sc.next();
+                ResultSet resultSet2 = connection.createStatement().executeQuery("SELECT * FROM users;");
+
+                while (resultSet2.next()) {
+                    if (resultSet2.getString("username").equals(deleteinput)) {
+                        username_in = "'"+ deleteinput+"'";
+                        name_in = resultSet2.getString("name");
+                        surname_in = resultSet2.getString("surname");
+                        age_in = resultSet2.getInt("age");
+                        break;
+                    } else {
+                        username_in = "NA";
+                        name_in = "";
+                        surname_in = "";
+                        age_in = 0;
+                    }
+                }
+            }
+        }
+
+        statement.close();
+    }
+
+
+
+}
